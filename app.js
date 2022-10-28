6ec83410da60752154537170c8a884b16633a175
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2022-10-27 22:10:40 -0300

    Servir contenido estático

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
-
 const app = express();
-
 const port = 8080;
+
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.send("Home page");
@@ -13,7 +13,7 @@ app.get("/hola-mundo", (req, res) => {
 });
 
 app.get("*", (req, res) => {
-  res.send("404 | Page not found");
+  res.sendFile(__dirname + "/public/404.html");
 });
 
 app.listen(port, () => {
